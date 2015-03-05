8897e9c78040f1a4ba163bd1d6ae81e80a36d1c3
Ed Maphis <emaphis85@gmail.com>
2015-03-04 22:11:50 -0500

    Flesh out functional programming chapter

--- brave-true/clojure-noob/src/clojure_noob/fun-prog.clj
+++ brave-true/clojure-noob/src/clojure_noob/fun-prog.clj
@@ -2,22 +2,28 @@
   (:require [midje.sweet :refer :all]))
 
 (facts  "pure functions:"
-  (fact (get [:chumbawumba] 0)
+  (fact
+    (get [:chumbawumba] 0)
     => :chumbawumba)
-  (fact (reduce + [1 10 5])
+  (fact
+    (reduce + [1 10 5])
     => 16)
-  (fact (str "wax on " "wax off")
+  (fact
+    (str "wax on " "wax off")
     => "wax on wax off"))
 
 (facts "pure functions are refentially transparent"
-  (fact (+ 1 2)
+  (fact
+    (+ 1 2)
     => 3))
 
 (defn wisdom
+  "the string ', Daniel-san' is immutable"
   [words]
   (str words ", Daniel-san"))
 
-(fact (wisdom "Always bathe on Fridays")
+(fact
+  (wisdom "Always bathe on Fridays")
   => "Always bathe on Fridays, Daniel-san")
 
 ;; always gives a differen result so not referntially transperent
@@ -27,6 +33,7 @@
     "You get a raise!"
     "Better luck next year!"))
 
+;; separate referencialy transparent functions from side-effecting procedures
 (defn analysis
   "analysis is a function"
   [text]
@@ -37,8 +44,14 @@
   [filename]
   (analysis (slurp filename)))  ;; slurp is not a function
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;(System/getProperty "user.dir")
+;;(analyze-file "project.clj")
+;;=> "Character count: 423"
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; living with immutable data structures
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn no-mutation
   [x]
@@ -51,27 +64,45 @@
   ;; outside of the let scope
   (println x))
 
-(no-mutation "Existential Angst Person")
-;; =>
+(fact "Kafka Human is in it's own scope"
+  (no-mutation "Existential Angst Person")
+  => nil)
 ;;Existential Angst Person
 ;;Kafka Human
 ;;Existential Angst Person
 
+
+;; recursive proplem solving:
 (defn sum
-  ([vals] (sum vals 0))
+  ([vals] (sum vals 0))  ;; --1-- default value for accumulator
   ([vals accumulating-total]
-    (if (empty? vals)
+    (if (empty? vals)    ;; --2-- empty vals is the base condition
       accumulating-total
       (sum (rest vals) (+ (first vals) accumulating-total)))))
 
-  (fact "test sum"
-    (sum []) => 0
-    (sum [1]) => 1
-    (sum [39 5 1]) => 45)
+(fact "evaluate sum"
+  (sum [39 5 1])   => 45   ;; single-arity - call 2-arity
+  (sum [39 5 1] 0) => 45
+  (sum [5 1] 39)   => 45
+  (sum [1] 44)     => 45
+  (sum [] 45)      => 45   ;; base case is reached, so return the accumulator
+  45               => 45 )
+
+;; should use 'recur' for more efficient looping
+(defn sum-r
+  ([vals]
+     (sum vals 0))
+  ([vals accumulating-total]
+     (if (empty? vals)
+       accumulating-total
+       (recur (rest vals) (+ (first vals) accumulating-total)))))
+
+(fact (sum-r [39 5 1]) => 45 )
 
 
-;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; function compostion instead of attribute mutation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require '[clojure.string :as s])
 
@@ -79,40 +110,59 @@
   [text]
   (s/replace (s/trim text) #"lol" "LOL"))
 
-(fact (clean "My boa constrictor is so sassy lol!  ")
+(fact
+  (clean "My boa constrictor is so sassy lol!  ")
   => "My boa constrictor is so sassy LOL!")
 
-(fact "pretty common style"
+;; use a chain of function calls instead of progressively mutating an object
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; cool things to do with functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fact "chaining function is pretty common style so function called 'comp'"
   ((comp s/lower-case s/trim) " Unclean string ")
   => "unclean string")
 
+;; 'def' create a function
+(def my-lower-trim-1 (comp s/lower-case s/trim))
+(fact
+  (my-lower-trim-1  " Unclean string ")
+  => "unclean string")
 
+;; an implementation of 'comp' that composes two functions
 (defn two-comp
   [f g]
   (fn [& args]
     (f (apply g args))))
 
-(def my-lower-trim (two-comp s/lower-case s/trim))
+(def my-lower-trim-2 (two-comp s/lower-case s/trim))
 
-(fact (my-lower-trim " Unclean string  ")
+(fact
+  (my-lower-trim-2 " Unclean string  ")
   => "unclean string")
 
 (def sum-times2 (two-comp  #(* 2 %) +))
 
-(fact (sum-times2 1 2 3)
+(fact
+  (sum-times2 1 2 3)
   => 12)
 
 ;; define a new version of 'comp'
 (defn my-comp
   [f & funs]
-  (if (empty? funs) f
+  (if (empty? funs) f    ;; base case - returned composed function
       (recur (fn [& args]
                  (f (apply (first funs) args)))
                (rest funs))))
 
-(fact ((my-comp s/lower-case s/trim) " Unclean string ")
+(fact "works like two-comp"
+  ((my-comp s/lower-case s/trim) " Unclean string ")
   => "unclean string")
-(fact ((my-comp s/reverse s/lower-case s/trim) " Unclean string ")
+
+(fact "works like regular 'comp'"
+  ((my-comp s/reverse s/lower-case s/trim) " Unclean string ")
   => "gnirts naelcnu")
 
 (def sum-times2' (two-comp  #(* 2 %) +))
@@ -120,8 +170,10 @@
 (fact (sum-times2' 1 2 3)
   => 12)
 
+
 ;;;;;;;;;;;;;;;;;;;;
 ;; memoization
+;;;;;;;;;;;;;;;;;;;;
 
 (defn sleepy-identity
   "returns the given value after a 1 second 'calculation'"
@@ -129,10 +181,19 @@
   (Thread/sleep 1000)
   x)
 
-(sleepy-identity "Mr. Fantatstico")
+(sleepy-identity "Mr. Fantatstico")    ;; after one second
 
 (def memo-sleepy-identity
-  "does calculation once, then returns imediatly"
+  "memoize does calculation once, then returns immediatly"
   (memoize sleepy-identity))
 
-(memo-sleepy-identity "Mr. Fantatstico")
+(memo-sleepy-identity "Mr. Fantatstico")  ;; after one second
+(memo-sleepy-identity "Mr. Fantatstico")  ;; immediately
+
+
+;; Pretty cool!
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Peg Thing - in it's own project
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

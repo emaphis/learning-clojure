6553389e2ed9c053a8d3d262d74b6182b1134767
Ed Maphis <emaphis85@gmail.com>
2015-03-11 17:49:29 -0400

    new playsync project for the core async chapter

---
+++ brave-true/playsync/src/playsync/core.clj
@@ -0,0 +1,10 @@
+(ns playsync.core
+  (:require [clojure.core.async
+             :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread
+                     alts! alts!! timeout]]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
